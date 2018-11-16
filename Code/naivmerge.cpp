83e900b37b9d35e5cbfdec137849670047ed81c5
jonas <jonas.kittelberger@gmail.com>
2018-11-16 16:27:03 +0100

    verbesserte Version, nur noch eine Arraykopie(am Ende vom mergesort)

--- Code/naivmerge.cpp
+++ Code/naivmerge.cpp
@@ -6,10 +6,9 @@
 #include <iostream>
 
 template<class Iterator, class Compare>
-void merge(Iterator startFirst, Iterator endFirst, Iterator startSecond, Iterator endSecond, Compare compare) {
+void merge(Iterator startFirst, Iterator endFirst, Iterator startSecond, Iterator endSecond, Iterator startExtra, Compare compare) {
     auto iFirst = startFirst;
     auto iSecond = startSecond;
-    int *startExtra = new int[endSecond - startFirst + 1];
     auto iExtra = startExtra;
 
     while (true) {
@@ -41,6 +40,7 @@ void merge(Iterator startFirst, Iterator endFirst, Iterator startSecond, Iterato
     }
 
     //Copy merged array to original positions
+    /*
     int *iOrig = startFirst;
     iExtra = startExtra;
     while (iOrig != endSecond) {
@@ -48,13 +48,19 @@ void merge(Iterator startFirst, Iterator endFirst, Iterator startSecond, Iterato
         iExtra++;
         iOrig++;
     }
-    delete(startExtra);
+     */
 }
 
 template<class Iterator, class Compare>
-void mergesort(Iterator startIndex, Iterator endIndex, Compare compare) {
+void mergesort(Iterator start, Iterator end, Compare compare) {
+    Iterator startIndex = start;
+    Iterator endIndex = end;
     int blockSize = 1;
     while (startIndex + blockSize < endIndex) {
+
+        int *startExtra = new int[endIndex - startIndex + 1];
+        auto iExtra = startExtra;
+
         //merge all blocks with size blockSize
         int *startIndexBlock1 = startIndex;
         int *endIndexBlock1 = startIndexBlock1 + blockSize;
@@ -63,8 +69,11 @@ void mergesort(Iterator startIndex, Iterator endIndex, Compare compare) {
                               ? endIndex
                               : (startIndexBlock2 + blockSize);
         while (true) {
-            merge(startIndexBlock1, endIndexBlock1, startIndexBlock2, endIndexBlock2, compare);
+            merge(startIndexBlock1, endIndexBlock1, startIndexBlock2, endIndexBlock2, iExtra, compare);
             if (endIndexBlock2 + blockSize >= endIndex) {
+                for(auto addEnd = endIndexBlock2; addEnd != endIndex; addEnd ++){
+                    *(startExtra + (addEnd - startIndex)) = *addEnd;
+                }
                 break;
             }
             startIndexBlock1 = endIndexBlock2;
@@ -73,7 +82,18 @@ void mergesort(Iterator startIndex, Iterator endIndex, Compare compare) {
             endIndexBlock2 = (startIndexBlock2 + blockSize) > endIndex
                              ? endIndex
                              : (startIndexBlock2 + blockSize);
+            iExtra = startExtra + (startIndexBlock1 - startIndex);
         }
         blockSize *= 2;
+
+        endIndex = startExtra + (endIndex - startIndex);
+        delete(startIndex);
+        startIndex = startExtra;
     }
+    auto copBack = start;
+    for(auto i = startIndex; i != endIndex; i ++){
+        *copBack = *i;
+        copBack ++;
+    }
+
 }
