d8714c2b0426487a953c7ae6a8df2883ecf9b1d9
unknown <patrick.sp@gmx.de>
2019-02-07 12:56:50 +0100

    Unnötige Zeilen gelöscht.

--- ChenAlgorithmus/bufferMerge.h
+++ ChenAlgorithmus/bufferMerge.h
@@ -38,35 +38,19 @@ namespace bufferMerge {
 
     //insertion sort for small lists
     template <typename Iterator, typename VecIterator>
-    void small_insertion_sort (Iterator begin_v, Iterator fin_v, VecIterator begin_m, bool i){
-        if (i) {
-            // Wenn i gesetzt ist sortiere innerhalb von v
-            for(auto it_i = begin_v + 1; it_i != fin_v; it_i++){
-                auto temp = *it_i;
-                Iterator it_j;
-                for (it_j = it_i; it_j != begin_v; it_j--) {
-                    if (*(it_j-1) > temp) {
-                        *it_j = *(it_j -1);
-                    } else {
-                        break;
-                    }
+    void small_insertion_sort (Iterator begin_v, Iterator fin_v, VecIterator begin_m) {
+        // Wenn i gesetzt ist sortiere innerhalb von v
+        for (auto it_i = begin_v + 1; it_i != fin_v; it_i++) {
+            auto temp = *it_i;
+            Iterator it_j;
+            for (it_j = it_i; it_j != begin_v; it_j--) {
+                if (*(it_j - 1) > temp) {
+                    *it_j = *(it_j - 1);
+                } else {
+                    break;
                 }
-                *it_j = temp;
             }
-        } else {
-            // Wenn i nicht gesetzt ist sortiere nach m
-            *begin_m = *begin_v;
-            for(auto it_i = begin_v + 1; it_i != fin_v; it_i++){
-                VecIterator it_j;
-                for (it_j = begin_m + (it_i - begin_v); it_j != begin_m; it_j--) {
-                    if (*(it_j-1) > *it_i) {
-                        *it_j = *(it_j -1);
-                    } else {
-                        break;
-                    }
-                }
-                std::swap(*it_j, *it_i);
-            }
+            *it_j = temp;
         }
     }
 
@@ -79,7 +63,6 @@ namespace bufferMerge {
     void recsort(Iterator begin_v, Iterator fin_v, VecIterator begin_m, bool i){
         int size = fin_v - begin_v;
         int pivot = (size - 1) / 2 + 1;
-        //TODO: Bedingung für smallsort bzw. gewünschten smallsort anpassen
         if(size > 60 || !i){
             recsort(begin_v, begin_v + pivot, begin_m, !i);
             recsort(begin_v + pivot, fin_v, begin_m + pivot, !i);
@@ -89,9 +72,7 @@ namespace bufferMerge {
                 merge(begin_v, fin_v, begin_v + pivot, begin_m);
             }
         } else {
-            //small_sort(begin_v, fin_v - 1, begin_m, i);
-            //heapSort(begin_v, fin_v);
-            small_insertion_sort(begin_v, fin_v, begin_m, i);
+            small_insertion_sort(begin_v, fin_v, begin_m);
         }
     }
 
