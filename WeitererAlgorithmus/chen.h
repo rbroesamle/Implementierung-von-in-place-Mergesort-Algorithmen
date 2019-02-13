a2336a033df8b6e64450cb2635e4feb9bc6b3f05
unknown <patrick.sp@gmx.de>
2019-02-14 00:20:24 +0100

    Merge verbessert. Scheint für kleinere (Teil-)Listen noch fehleranfällig zu sein. Momentan abgefangen durch ChenSort für weniger als 3000 Elemente. Läuft damit offenbar zuverlässig.

---
+++ WeitererAlgorithmus/chen.h
@@ -0,0 +1,156 @@
+//
+// Created by Patrick on 23.12.2018.
+//
+#include <iterator>
+#include <cmath>
+#include <algorithm>
+#include "bufferMerge.h"
+
+namespace chen_sort {
+    template<typename Iterator>
+    void mergesort_chen(Iterator s, Iterator e);
+
+    //finds the minimum in range *s to *e-1
+    template<typename Iterator>
+    Iterator find_minimum(Iterator s, Iterator e) {
+        Iterator min = s;
+        for (Iterator it = s; it != e; it++) {
+            if (*it < *min) {
+                min = it;
+            }
+        }
+        return min;
+    }
+
+    template<typename Iterator>
+    Iterator findNextXBlock(Iterator x_0, Iterator z, Iterator y, int k, int f, Iterator b_1, Iterator b_2) {
+        Iterator x; //return Iterator
+        typename std::iterator_traits<Iterator>::value_type min_1, min_2;
+        int t = ((z - x_0) - f) / k;
+        Iterator m = x_0 + (f + (k * t));
+        if (!(z < m)) m = m + k;
+        Iterator i = m;
+        Iterator j;
+        bool minNotSet = true; // is false, when  min_1/2 are set. maybe come up with a more elegant solution
+        while ((i + k) <= y) {
+            if (i != b_1 && i != b_2) {
+                if (i < b_1 && b_1 < i + k) j = m - 1; else j = i + (k - 1);
+                // check if min1/2 are set. It should work just like infinity in pseudo-code
+                if (minNotSet || (!(min_1 < *i) && !(min_2 < *j))) {
+                    x = i;
+                    min_1 = *i;
+                    min_2 = *j;
+                    minNotSet = false;
+                }
+            }
+            i = i + k;
+        }
+        return x;
+    }
+
+
+    template<typename Iterator>
+    void mergeBandY(Iterator z, Iterator y, Iterator y_n) {
+        Iterator j;
+        typename std::iterator_traits<Iterator>::value_type t;
+        while (z <= y && y < y_n) {
+            j = find_minimum(z, y);
+            if (!(*y < *j)) {
+                t = *z;
+                *z = *j;
+                *j = t;
+            } else {
+                t = *z;
+                *z = *y;
+                *y = t;
+                y++;
+            }
+            z++;
+        }
+        //TODO: mit rekursivem chen-aufruf statt heapsort experimentieren
+        //if (z < y) heapSort(z, y_n);
+        if (z < y) mergesort_chen(z, y_n);
+    }
+
+
+    template<typename Iterator>
+    void merge(Iterator x_0, Iterator y_0, Iterator y_n, int k, int recursionDepth) {
+        // Initialization. (line 1-4 in pseudo-code)
+        int f = (y_0 - x_0) % k;
+        Iterator x;
+        if (f == 0) x = y_0 - (2 * k); else x = (y_0 - (k + f));
+        if (x < x_0) x = x_0; //optional line to make the merge step work with y_0 - x_0 < 2k as mentioned in the paper
+        typename std::iterator_traits<Iterator>::value_type t = *x;
+        *x = *x_0;
+        Iterator z = x_0, y = y_0, b_1 = x + 1, b_2 = y_0 - k;
+        // line 5
+        while (y - z > 2 * k) {
+            // line 6 -14
+            if (!(*y < *x) || y_n == y) {
+                *z = *x;
+                *x = *b_1;
+                x++;
+                if ((x - x_0) % k == f) {
+                    if (z < x - k) b_2 = x - k;
+                    x = findNextXBlock(x_0, z, y, k, f, b_1, b_2);
+                }
+            } else {
+                *z = *y;
+                *y = *b_1;
+                y++;
+                if ((y - y_0) % k == 0) b_2 = y - k;
+            }
+            // line 15
+            z++;
+            *b_1 = *z;
+            if (z == x) x = b_1;
+            if (z == b_2) b_2 = y_n; // Use end-Iterator to replace "-1"
+            b_1++;
+            if ((b_1 - x_0) % k == f) {
+                if (b_2 == y_n) b_1 = b_1 - k; else b_1 = b_2;
+            }
+        }
+        //copy temp back, merge B and Y
+        *z = t;
+        if (recursionDepth > 1) mergeBandY(z, y, y_n);
+        else {
+            //TODO: mit rekursivem chen-aufruf statt heapsort experimentieren
+            mergesort_chen(z, y);
+            //heapSort(z, y);
+            merge(z, y, y_n, static_cast<int>(std::sqrt(k)), recursionDepth + 1);
+        }
+    }
+
+
+    template<typename Iterator>
+    void smallInsertionSort(Iterator s, Iterator e) {
+        typename std::iterator_traits<Iterator>::value_type temp;
+        for (auto it_i = s + 1; it_i != e; it_i++) {
+            temp = *it_i;
+            Iterator it_j;
+            for (it_j = it_i; it_j != s; it_j--) {
+                if (*(it_j - 1) > temp) {
+                    *it_j = *(it_j - 1);
+                } else {
+                    break;
+                }
+            }
+            *it_j = temp;
+        }
+    }
+
+
+    template<typename Iterator>
+    void mergesort_chen(Iterator s, Iterator e) {
+        int size = e - s;
+        int k = static_cast<int>(std::sqrt(size));
+        int pivot = (size - 1) / 2;
+        if (size > 50) {
+            bufferMerge::mergesort(s, s + pivot, s + pivot);
+            mergesort_chen(s + pivot, e);
+            merge(s, s + pivot, e, k, 0);
+        } else {
+            bufferMerge::small_insertion_sort(s, e, e);
+        }
+    }
+}
--- WeitererAlgorithmus/huang_langston_merge.h
+++ WeitererAlgorithmus/huang_langston_merge.h
@@ -3,10 +3,9 @@
 //
 #include <cmath>
 #include <algorithm>
-#include "bufferMerge.h"
+#include "chen.h"
 
 namespace huang_langston_merge{
-
     template <typename Iterator>
     void mergesort(Iterator s, Iterator e);
 
@@ -25,9 +24,9 @@ namespace huang_langston_merge{
      * returns the leftmost unmerged element
      */
     template<typename Iterator>
-    Iterator merge_with_buffer(Iterator buffer, Iterator first_series, Iterator second_series, Iterator e){
+    Iterator merge_with_buffer(Iterator buffer, Iterator first_series, Iterator second_series){
         Iterator first = first_series, second = second_series;
-        while(first != second_series && second != e){
+        while(first != second_series){
             if (*second < *first){
                 std::swap(*second, *buffer);
                 second++;
@@ -46,8 +45,7 @@ namespace huang_langston_merge{
     template<typename Iterator>
     Iterator find_series(Iterator s, Iterator e, int blockSize){
         //int block_count = static_cast<int>(std::round(static_cast<float>((e-s)) / static_cast<float>(blockSize)));
-        //TODO :blocksize beachten
-        int block_count = ((e-s)+(blockSize-1)) / blockSize;
+        int block_count = (e-s) / blockSize;
         for (int i = 1; i < block_count; i++){
             int head_distance = i * blockSize;
             if (s[head_distance] < s[head_distance - 1]) return s + head_distance;
@@ -73,6 +71,45 @@ namespace huang_langston_merge{
             if (min != current) swap_blocks(min, current, blockSize);
         }
     }
+
+
+    /*
+     * places the sqrt(e-s) largest elements at the begin of the list, followed by the remainders of
+     * the sublists
+     */
+    template<typename Iterator>
+    void extract_buffer(Iterator s, Iterator m, Iterator e, int blockSize){
+        int list_1_shifts = 1, list_2_shifts = 1;
+        for(int i = 0; i < blockSize; i++){
+            if(*(e-list_2_shifts) < *(m-list_1_shifts)) list_1_shifts++; else list_2_shifts++;
+        }
+        list_1_shifts--, list_2_shifts--;
+        std::rotate(m, e - list_2_shifts,e);
+        std::rotate(s, (m - list_1_shifts),m+list_2_shifts);
+    }
+
+    /*
+     * basic merge procedure for two sublist of length (m-s) + (e-m) = n
+     * each list must contain an integral multiple of sqrt(n)
+     * Edit: At this point, the buffer has already been extracted
+     */
+    template<typename Iterator>
+    void basic_inplace_merge(Iterator s, Iterator e, int blockSize){
+        sort_blocks(s+blockSize, e, blockSize);
+        Iterator second_series = find_series(s + blockSize, e, blockSize);
+        Iterator first_series = merge_with_buffer(s, s+blockSize, second_series);
+        second_series = find_series(second_series, e, blockSize);
+        while (second_series != e){
+            first_series = merge_with_buffer(first_series - blockSize, first_series, second_series);
+            second_series = find_series(second_series, e, blockSize);
+        }
+        std::rotate(first_series - blockSize, first_series,e);
+        //TODO: replace std::sort (possibly by recursive mergesort call)
+        //chen_sort::mergesort_chen(e-blockSize, e);
+        //std::sort(e - blockSize, e);
+        huang_langston_merge::mergesort(e - blockSize, e);
+    }
+
 
     /*
      * merges F and G using the Buffer and puts H in its final position
@@ -100,80 +137,93 @@ namespace huang_langston_merge{
         swap_blocks(s, b, t_1);
     }
 
+    /*
+     * binary search for the position in list [s...e] to insert *element
+     * if the whole list is smaller than element, e is returned
+     */
+    template <typename Iterator>
+    Iterator binary_search(Iterator s, Iterator e, Iterator element){
+        Iterator pos;
+        while(e-s > 1){
+            pos = s + ((e-s)/2);
+            if (*pos == *element) return pos;
+            if (*pos < *element) s = pos + 1; else e = pos;
+        }
+        if (*s < *element) return e; else return s;
+    }
 
     /*
-     * places the sqrt(e-s) largest elements at the begin of the list, followed by the remainders of
-     * the sublists
-     * the case that the sublists do not meet the size requirements is handled here
+     * merges a large sorted list with a far smaller list
+     * since this uses rotations, the smaller list must begin at m, with m being the position of the last element of
+     * the larger sublist +1
      */
-    template<typename Iterator>
-    Iterator extract_buffer(Iterator s, Iterator m, Iterator e, int blockSize, Iterator &block_e){
+    template <typename Iterator>
+    void block_merge_backward(Iterator s, Iterator m, Iterator e){
+        for (int rightListSize = (e - m); rightListSize > 0; rightListSize--){
+            Iterator insert_pos = binary_search(s, m, m + (rightListSize-1));
+            std::rotate(insert_pos, m, e);
+            m = insert_pos; e = insert_pos + (rightListSize-1);
+        }
+    }
+
+
+    /*
+     * merge procedure doing some preparations and then calling the basic algorithm
+     * each sublist must have at least sqrt(n) elements
+     */
+    template <typename Iterator>
+    void merge(Iterator s, Iterator m, Iterator e) {
+        int blockSize = std::sqrt(e - s);
         int s_1 = 1, s_2 = 1;
-        for(int i = 0; i < blockSize; i++){
-            if(*(e-s_2) < *(m-s_1)) s_1++; else s_2++;
+        for (int i = 0; i < blockSize; i++) {
+            if (*(e - s_2) < *(m - s_1)) s_1++; else s_2++;
         }
         s_1--, s_2--;
-        int d = ((e-m) - s_2) % blockSize;
-        if (d == 0 && ((m- s) - s_1)%blockSize == 0) {
+        int d = ((e - m) - s_2) % blockSize;
+        if (d == 0 && ((m - s) - s_1) % blockSize == 0) {
             std::rotate(m, e - s_2, e);
             std::rotate(s, (m - s_1), m + s_2);
-            block_e = e;
-            return s;
+            //TODO: extract buffer step has already been done, so it must be removed from the basic_merge, which must receive the blockSize
+            basic_inplace_merge(s, e, blockSize);
         }
-        swap_blocks(m -(s_1 + s_2), e - s_2, s_2);
+        swap_blocks(m - (s_1 + s_2), e - s_2, s_2);
         //TODO merge b and c using the buffer instead
-        huang_langston_merge::mergesort(e - (s_2+d), e);
+        //chen_sort::mergesort_chen(e - (s_2 + d), e);
+        //std::sort(e - (s_2 + d), e);
+        huang_langston_merge::mergesort(e - (s_2 + d), e);
         int t_1 = (m - s) % blockSize;
-        if (t_1 != 0) mergeFandG(s, s+t_1, m, m+blockSize, m-t_1);
-        swap_blocks(s + t_1, m-blockSize, blockSize);
-        block_e = e - (s_2+d);
-        return s+t_1;
-    }
-
-    /*
-     * basic merge procedure for two sublist of length (m-s) + (e-m) = n
-     * each list must contain an integral multiple of sqrt(n)
-     */
-    template<typename Iterator>
-    void inplace_merge(Iterator s, Iterator m, Iterator e){
-        int blockSize = static_cast<int>(std::round(std::sqrt(e-s)));
-        Iterator block_e = e;
-        s = extract_buffer(s, m, e, blockSize, block_e);
-        //changed e to block_e in some calls
-        sort_blocks(s+blockSize, block_e, blockSize);
-        Iterator second_series = find_series(s + blockSize, e, blockSize);
-        Iterator first_series = merge_with_buffer(s, s+blockSize, second_series, e);
-        second_series = find_series(second_series, e, blockSize);
-        while (second_series != e){
-            first_series = merge_with_buffer(first_series - blockSize, first_series, second_series, e);
-            second_series = find_series(second_series, e, blockSize);
+        if (t_1 != 0) mergeFandG(s, s + t_1, m, m + blockSize, m - t_1);
+        swap_blocks(s + t_1, m - blockSize, blockSize);
+        Iterator block_e = e - (s_2 + d);
+        if(e-block_e == blockSize){
+            basic_inplace_merge(s+t_1, e, blockSize);
+        } else {
+            if (e-block_e > blockSize) {
+                // rotate the largest block-sized part of block E in correct block-position and merge ignoring the rest of E. merge thhe rest later
+                std::rotate(block_e, e - blockSize, e);
+                basic_inplace_merge(s + t_1, block_e + blockSize, blockSize);
+                block_merge_backward(s+t_1, block_e + blockSize, e);
+                //std::sort(s, e);
+            } else {
+                basic_inplace_merge(s+t_1, block_e, blockSize);
+                block_merge_backward(s+t_1, block_e, e);
+                //std::sort(s, e);
+            }
         }
-        //TODO check if std::rotate is inplace
-        std::rotate(first_series - blockSize, first_series,e);
-        //TODO: replace std::sort (possibly by recursive mergesort call)
-        huang_langston_merge::mergesort(e-(3*blockSize), e);
-        //std::sort(e - blockSize, e);
-        //std::sort(e - (2* blockSize), e - blockSize);
-        //huang_langston_merge::mergesort(e - blockSize, e);
-        //huang_langston_merge::mergesort(e - (2* blockSize), e - blockSize);
     }
 
-    /*
-     * standard recursive merge procedure, using normal mergesort with the rest of the list as internal buffer for
-     * each left sublist and in-place merge when no buffer is available
-     */
+
     template <typename Iterator>
     void mergesort(Iterator s, Iterator e){
         int size = e - s;
         int pivot = (size - 1) / 2;
-        if(size > 50){
+        if(size > 3000){
             bufferMerge::mergesort(s, s+pivot, s+pivot);
             huang_langston_merge::mergesort(s + pivot, e);
-            huang_langston_merge::inplace_merge(s, s + pivot, e);
+            huang_langston_merge::merge(s, s + pivot, e);
         } else {
-            if (size > 1) {
-                bufferMerge::small_insertion_sort(s, e, e);
-            }
+            //bufferMerge::small_insertion_sort(s, e, e);
+            chen_sort::mergesort_chen(s, e);
         }
     }
 }
