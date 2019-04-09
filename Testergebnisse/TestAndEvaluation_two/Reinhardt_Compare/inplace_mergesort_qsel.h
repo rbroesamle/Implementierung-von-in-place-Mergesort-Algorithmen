22a260e64850786db40200af337d653d26038c29
jonas <jonas.kittelberger@gmail.com>
2019-04-09 19:25:36 +0200

    Weitere Diagramme Daten/Werte überprüfen (lokal)

--- Testergebnisse/TestAndEvaluation_two/Reinhardt_Compare/inplace_mergesort_qsel.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by jonas on 02.01.2019.
-//
-template <typename Iterator, typename Compare>
-void in_place_mergesort(Iterator begin, Iterator fin, Compare comp);
-
-template <typename Iterator, typename Compare>
-void rec_reinhardt_left_gap(Iterator start_gap, Iterator start_list, Iterator end_list, Compare comp);
